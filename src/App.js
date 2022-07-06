e3e442ec990ed86bd45c8229c26db0d94848aae1
Thomas Pendleton <thomas6576@yahoo.com>
2022-07-06 11:52:47 -0700

    finished project

--- src/App.js
+++ src/App.js
@@ -14,22 +14,51 @@ function App() {
   const [person, setPerson] = useState(null)
   const [title, setTitle] = useState('name')
   const [value, setValue] = useState('random person')
-  
+
   const fetchRandomUser = async () => {
     const response = await fetch(url)
     const data = await response.json()
-    console.log(data.results);
-  }
-  useEffect(() => {
-    const user = fetchRandomUser()
-    setPerson(user)
-    
-  }, [])
-  
+    const person = data.results[0]
+    const { phone, email } = person
+    const { large: image } = person.picture
+    const {
+      login: { password },
+    } = person
+    const { first, last } = person.name
+    const { age } = person.dob
+    const {
+      street: { number, name },
+    } = person.location
+    const newPerson = {
+      phone,
+      email,
+      image,
+      password,
+      age,
+      name: `${first} ${last}`,
+      street: `${number} ${name}`,
+    }
 
+    setPerson(newPerson)
+    setLoading(false)
+    setTitle('name')
+    setValue(newPerson.name)
+  }
+
+  useEffect(() => {
+    fetchRandomUser()
+  }, [])
 
   const handleValue = (e) => {
-    console.log(e.target)
+    if (e.target.classList.contains('icon')) {
+      const newValue = e.target.dataset.label
+      setTitle(newValue)
+      setValue(person[newValue])
+    }
+  }
+
+  const handleClick = () => {
+    fetchRandomUser()
   }
 
   return (
@@ -37,19 +66,55 @@ function App() {
       <div className="block bcg-black"></div>
       <div className="block">
         <div className="container">
-          <img src={(person && person.image) || defaultImage} alt='random user' className='user-img' />
-          <p className="user-title">my {title} is</p>
+          <img
+            src={(person && person.image) || defaultImage}
+            alt="random user"
+            className="user-img"
+          />
+          <p className="user-title">My {title} is</p>
           <p className="user-value">{value}</p>
           <div className="values-list">
-            <button className="icon" data-label='name' onMouseOver={handleValue}><FaUser /></button>
-            <button className="icon" data-label='email' onMouseOver={handleValue}><FaEnvelopeOpen /></button>
-            <button className="icon" data-label='age' onMouseOver={handleValue}><FaCalendarTimes /></button>
-            <button className="icon" data-label='location' onMouseOver={handleValue}><FaMap /></button>
-            <button className="icon" data-label='phone' onMouseOver={handleValue}><FaPhone /></button>
-            <button className="icon" data-label='password' onMouseOver={handleValue}><FaLock /></button>
+            <button
+              className="icon"
+              data-label="name"
+              onMouseOver={handleValue}
+            >
+              <FaUser />
+            </button>
+            <button
+              className="icon"
+              data-label="email"
+              onMouseOver={handleValue}
+            >
+              <FaEnvelopeOpen />
+            </button>
+            <button className="icon" data-label="age" onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+            <button
+              className="icon"
+              data-label="street"
+              onMouseOver={handleValue}
+            >
+              <FaMap />
+            </button>
+            <button
+              className="icon"
+              data-label="phone"
+              onMouseOver={handleValue}
+            >
+              <FaPhone />
+            </button>
+            <button
+              className="icon"
+              data-label="password"
+              onMouseOver={handleValue}
+            >
+              <FaLock />
+            </button>
           </div>
-          <button className="btn" type='button'>
-            {loading? 'loading...': 'random user'}
+          <button className="btn" type="button" onClick={handleClick}>
+            {loading ? 'loading...' : 'random user'}
           </button>
         </div>
       </div>
